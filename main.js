001915f16c67e033b7c8736c1a7934d696f9c4b0
zcawood333 <zcawood@purdue.edu>
2021-06-30 15:24:06 -0400

    basic app.post works

--- main.js
+++ main.js
@@ -3,9 +3,16 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const debug = true;
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+app.get('/files', (req, res) => {
+    if(debug) console.log('GET request...');
+    res.send('this will be a download');
+});
+
+app.post('/upload', (req, res) => {
+    if(debug) console.log('POST request...');
+    res.send('this will be an upload');
 });
 
 app.listen(port);
