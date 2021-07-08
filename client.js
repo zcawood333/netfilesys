659eb0148f5ecdfdb6a8d1e6e480ae42303159d6
zcawood333 <zcawood@purdue.edu>
2021-07-08 15:34:19 -0400

    can now send a multicast message

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 //command line client utility; sends get or post requests
-//keyword args: --method=[g,get,p,post], --hostname=..., --port=..., --path=..., --fpath=...
+//keyword args: --multicast='message', --method=[g,get,p,post], --hostname=..., --port=..., --path=..., --fpath=...
+//multicast: will send a multicast message to default address
 //method: determines whether http request is GET or POST request
 //hostname: hostname to send the request to
 //port: port to send the request to
@@ -10,6 +11,10 @@ const http = require('http');
 const formData = require('form-data');
 const form = new formData();
 const fs = require('fs');
+const dgram = require('dgram');
+
+
+
 
 //Defaults
 let port = 5000;
@@ -20,63 +25,89 @@ let fpath = null;
 let postFile = null;
 let get = true;
 let post = false;
+let multicastMsg = null;
 
 //testing
 const debug = false;
 
-//argv handling and error checking
 if (debug) {console.log(argv)};
-if (argv.hostname) {
-    hostname = argv.hostname;
-}
-if (argv.path) {
-    path = argv.path;
-}
-if (argv.port) {
-    port = argv.port;
-}
-if (argv.method && (argv.method === 'p' || argv.method === 'post' || argv.method === 'get' || argv.method === 'g')) {
-    method = argv.method;
-}
+if (argv.multicast) {
+    multicastMsg = argv.multicast;
+    const server = dgram.createSocket('udp4');
+    const udpHostPort = 5001;
+    const udpSendPort = 5002;
+    const multicastAddr = '230.185.192.108';
 
-const options = {
-    hostname: hostname,
-    port: port,
-    path: path,
-    method: 'GET'
-}
-
-//make POST-specific changes
-if (method.toLowerCase() === 'post' || method.toLowerCase() === 'p') {
-    post = true;
-    get = false;
-    options.method = 'POST';
-    if (!(argv.fpath)) {
-        throw new Error('this post request requires a file path');
+    server.bind(udpHostPort, undefined, () => {
+        server.setBroadcast(true);
+        server.setMulticastTTL(128);
+        server.addMembership(multicastAddr); 
+        broadcastNew();
+    });
+    
+    function broadcastNew() {
+        const message = new Buffer.from(multicastMsg);
+        server.send(message, 0, message.length, udpSendPort, multicastAddr, err => {
+            if (err && debug) {console.log(err)};
+            server.close();
+        });
+        console.log("Sent " + message);
+        
     }
-    fpath = argv.fpath;
-    postFile = fs.createReadStream(fpath);
-    form.append('fileKey', postFile);
-    options.headers = form.getHeaders();
-}
-
-const req = http.request(options);
-
-req.on('error', error => {
-    console.error(error)
-});
-
-req.on('response', res => {
-    console.log(`statusCode: ${res.statusCode}`)
-
-    res.on('data', d => {
-        console.log(d.toString());
-    })
-});
-
-if (post) {
-    //request end is implicit after piping form
-    form.pipe(req);
 } else {
-    req.end();
+    //argv handling and error checking
+    if (argv.hostname) {
+        hostname = argv.hostname;
+    }
+    if (argv.path) {
+        path = argv.path;
+    }
+    if (argv.port) {
+        port = argv.port;
+    }
+    if (argv.method && (argv.method === 'p' || argv.method === 'post' || argv.method === 'get' || argv.method === 'g')) {
+        method = argv.method;
+    }
+
+    const options = {
+        hostname: hostname,
+        port: port,
+        path: path,
+        method: 'GET'
+    }
+
+    //make POST-specific changes
+    if (method.toLowerCase() === 'post' || method.toLowerCase() === 'p') {
+        post = true;
+        get = false;
+        options.method = 'POST';
+        if (!(argv.fpath)) {
+            throw new Error('this post request requires a file path');
+        }
+        fpath = argv.fpath;
+        postFile = fs.createReadStream(fpath);
+        form.append('fileKey', postFile);
+        options.headers = form.getHeaders();
+    }
+
+    const req = http.request(options);
+
+    req.on('error', error => {
+        console.error(error)
+    });
+
+    req.on('response', res => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', d => {
+            console.log(d.toString());
+        })
+    });
+
+    if (post) {
+        //request end is implicit after piping form
+        form.pipe(req);
+    } else {
+        req.end();
+    }
 }
